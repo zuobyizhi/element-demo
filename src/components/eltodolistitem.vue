0c9e7ddd90427480b68e712f3621726ecca54ffe
zuobyizhi <zuobyige@sina.cn>
2017-09-21 17:45:13 +0800

    样式调整

--- src/components/eltodolistitem.vue
+++ src/components/eltodolistitem.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="item-white-block item gray-thin-border">
     <div class="head-pic-div img-scale float-left"><img class="img-self-adaptation" v-lazy="img"></img></div>
-    <div class="msg-div float-left">
-      <h3 class="item-title">{{shorten(content, 16, '...')}}</h3>
-      <div class="item-content line-feed">{{shorten(content, 100, '...')}}</div>
-      <div class="item-bottom">
+    <div class="msg-div float-left pos-relative">
+      <h3 class="item-title title-font-size align-left">{{shorten(content, 16, '...')}}</h3>
+      <div class="item-content line-feed content-font-size font-color-dark-gray align-left">{{shorten(content, 100, '...')}}</div>
+      <div class="item-bottom tip-font-size font-color-light-gray">
         <ul>
           <div class="float-left margin-left-10px"><i class="el-icon-time"></i></div>
           <div class="float-left margin-left-5px">{{time}}</div>
           <div class="float-left margin-left-10px">{{type}}</div>
-          <div class="float-left margin-left-20px"><button class="btn" @click="updateItem">修改</button></div>
-          <div class="float-left margin-left-5px"><button class="btn" @click="deleteItem">删除</button></div>
+          <div class="float-left margin-left-20px"><button class="a-btn" @click="updateItem">修改</button></div>
+          <div class="float-left margin-left-5px"><button class="a-btn" @click="deleteItem">删除</button></div>
         </ul>
       </div>
     </div>
@@ -51,23 +51,12 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .item-title {
-  text-align: left;
   margin-bottom: 10px;
   font-weight: 700;
-  font-size: 18px;
-  line-height: 27px;
 }
 .head-pic-div {
   height: 158px;
   width: 160px;
-}
-.btn {
-  color: #20a0ff;
-  background-color: #fff;
-  border-color: #fff;
-  padding: 0;
-  font-size: 12px;
-  border-width: 0;
 }
 .item-white-block {
   background-color: #fff;
@@ -76,10 +65,6 @@ export default {
 .item-content {
   margin: 5px;
   left: 0;
-  color: #666;
-  font-size: 14px;
-  line-height: 23px;
-  text-align: left;
 }
 .item {
   width: 100%;
@@ -88,14 +73,11 @@ export default {
 .msg-div {
   width: 400px;
   height: 160px;
-  position: relative;
   padding: 12px 16px;
 }
 .item-bottom {
   position: absolute;
   bottom: 12px;
   left: 12px;
-  font-size: 12px;
-  color: #999;
 }
 </style>
